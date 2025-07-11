e54673385da1ddb42cc9312b5800b6ddb3842d7d
Surve <surve8729@gmail.com>
2025-07-11 21:59:10 +0530

    Day 6 View Data and Route wildcards

---
+++ resources/views/job.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        {{$job['title']}}
+    </x-slot:heading>
+    <h3>This job will pay you {{$job['salary']}} per year.</h3>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,50 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => 'Software Engineer',
+                'salary' => '$100,000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Data Engineer',
+                'salary' => '$80,000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'AI Engineer',
+                'salary' => '$200,000'
+            ],
+        ]
+    ]);
+});
+
+Route::get('/jobs/{id}', function ($id) {
+    $jobs = [
+        [
+            'id' => 1,
+            'title' => 'Software Engineer',
+            'salary' => '$100,000'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Data Engineer',
+            'salary' => '$80,000'
+        ],
+        [
+            'id' => 3,
+            'title' => 'AI Engineer',
+            'salary' => '$200,000'
+        ],
+    ];
+
+    $job = \Illuminate\Support\Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+    return view('job', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
